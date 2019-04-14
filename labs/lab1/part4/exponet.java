39ac7f42ce529eae14d97fdb6ffb01554566993e
Victor Delaplaine <vdelaplaines@aol.com>
2019-04-14 11:05:01 -0700

    autoadd

--- labs/lab1/part4/exponet.java
+++ labs/lab1/part4/exponet.java
@@ -1,30 +1,32 @@
+import java.util.*;
 
-uble exp(long base, long exponent) {
+public class exponet{
+	public static void main(String [] args)
+	{
+		System.out.println("2^8="+pow(2,8));
+		System.out.println("8^2="+pow(8,2));
+		System.out.println("10^10="+pow(10,10));
+	}
 
-	    //when the exponent is zero
-		//    if (exponent == 0) return 1.0;
-		//
-		//        long result = base;
-		//            long exponent_abs = Math.abs(exponent);
-		//
-		//                //repeating multiplication many times to achieve exponentiation
-		//                    for (int i = 2 ; i <= exponent_abs ; ++i) {
-		//
-		//                            result = multiply(result, base);
-		//                                }
-		//
-		//                                     //if the exponent is positive, return result you found.
-		//                                          if (exponent > 0) return result;
-		//                                               //if it is negative, return a fraction.
-		//                                                    return 1/((double)result);
-		//                                                      }
-		//
-		//                                                      //repeating addition many times to achieve multiplication.
-		//                                                      long multiply(long number1, long number2){
-		//                                                          long result = number1;
-		//
-		//                                                              for (int i = 1; i < number2; ++i){
-		//                                                                      result += number1;
-		//                                                                          }
-		//                                                                              return result;
-		//                                                                               }k:w
+	public static int pow(int base, int exponet)
+	{
+		//special case when exp is zero
+		if (exponet==0) return 1;
+
+		int result  = base;
+
+		//multiplying repeadily is same thing as x^y = x*....*x ytimes
+		for (int i=1; i< exponet; i++)
+			result = mult(result,base); //start at i = 1 because it has done two multiplications
+		return result;
+	}
+
+	public static int  mult(int num1, int num2)
+	{
+		int prod = 0;
+		for (int i=0; i<num2; i++)
+			prod=prod+num1;
+
+		return prod;
+	}	
+}
