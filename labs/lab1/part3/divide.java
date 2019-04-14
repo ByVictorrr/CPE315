d78de1e70d07a1640cfccd9dbb71e8c398a105f0
Victor Delaplaine <vdelaplaines@aol.com>
2019-04-14 04:05:02 -0700

    autoadd

---
+++ labs/lab1/part3/divide.java
@@ -0,0 +1,11 @@
+import java.util.*; 
+
+public class divide{ 
+
+	public static void main(String []args){
+	}
+	public static int pow (int x,int y ) {
+		// x*z = x+x+...+x_ztimes
+	}
+
+}
--- labs/lab1/part4/exponet.java
+++ labs/lab1/part4/exponet.java
@@ -1,33 +1,30 @@
-import java.util.*; public class exponet{ 
-	public static void main(String []args){
-		System.out.println("8^0=" + pow(8,0));
-		System.out.println("8^1=" + pow(8,1));
-		System.out.println("3^8=" + pow(3,8));
-		System.out.println("10^10=" + pow(10,10));
-		System.out.println(Math.pow(10,10));
-	}
-	public static int pow (int x,int y ) {
-		// x*z = x+x+...+x_ztimes
-		// x^y = x*x*......x_ytimes
-		int prod=0;
-	    int	exp = 0;
-	//
-		if(y==0) 
-			return 1;
-		else if (x==0)
-			return 0;
-		else if (y==1)
-			return x;
-		else{
-			while (y>1){ 
-			for(int i = 0; i<x; i++) //this is x*x
-			{ 
-				prod += x;
-			}
-				y--;
-			}
-		}
-		return prod;
-	}
 
-}
+uble exp(long base, long exponent) {
+
+	    //when the exponent is zero
+		//    if (exponent == 0) return 1.0;
+		//
+		//        long result = base;
+		//            long exponent_abs = Math.abs(exponent);
+		//
+		//                //repeating multiplication many times to achieve exponentiation
+		//                    for (int i = 2 ; i <= exponent_abs ; ++i) {
+		//
+		//                            result = multiply(result, base);
+		//                                }
+		//
+		//                                     //if the exponent is positive, return result you found.
+		//                                          if (exponent > 0) return result;
+		//                                               //if it is negative, return a fraction.
+		//                                                    return 1/((double)result);
+		//                                                      }
+		//
+		//                                                      //repeating addition many times to achieve multiplication.
+		//                                                      long multiply(long number1, long number2){
+		//                                                          long result = number1;
+		//
+		//                                                              for (int i = 1; i < number2; ++i){
+		//                                                                      result += number1;
+		//                                                                          }
+		//                                                                              return result;
+		//                                                                               }k:w
