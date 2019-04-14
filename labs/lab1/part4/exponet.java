5bcba9c845dbe61bfa47f7a3227845eba21c4b48
Victor Delaplaine <vdelaplaines@aol.com>
2019-04-14 03:05:02 -0700

    autoadd

--- labs/lab1/part4/exponet.java
+++ labs/lab1/part4/exponet.java
@@ -1,19 +1,33 @@
-import java.util.*;
-
-public class exponet{
-
+import java.util.*; public class exponet{ 
 	public static void main(String []args){
 		System.out.println("8^0=" + pow(8,0));
 		System.out.println("8^1=" + pow(8,1));
 		System.out.println("3^8=" + pow(3,8));
 		System.out.println("10^10=" + pow(10,10));
+		System.out.println(Math.pow(10,10));
 	}
-	public static int pow (final int base, final int exponent) {
-	  int result = base;
-	  for (int i = 1; i < exponent; i++)
-	     for (int j = 0; j < base; j++)
-	         result += base;
-	  return result;
+	public static int pow (int x,int y ) {
+		// x*z = x+x+...+x_ztimes
+		// x^y = x*x*......x_ytimes
+		int prod=0;
+	    int	exp = 0;
+	//
+		if(y==0) 
+			return 1;
+		else if (x==0)
+			return 0;
+		else if (y==1)
+			return x;
+		else{
+			while (y>1){ 
+			for(int i = 0; i<x; i++) //this is x*x
+			{ 
+				prod += x;
+			}
+				y--;
+			}
+		}
+		return prod;
 	}
 
 }
