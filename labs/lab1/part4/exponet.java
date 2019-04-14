2e5fc9f52d23445f5734d04240724d513def1ce4
Victor Delaplaine <vdelaplaines@aol.com>
2019-04-14 11:08:29 -0700

    autoadd

--- labs/lab1/part4/exponet.java
+++ labs/lab1/part4/exponet.java
@@ -5,7 +5,9 @@ public class exponet{
 	{
 		System.out.println("2^8="+pow(2,8));
 		System.out.println("8^2="+pow(8,2));
-		System.out.println("10^10="+pow(10,10));
+		System.out.println("5^6="+pow(5,6));
+		System.out.println("1^6="+pow(1,6));
+		System.out.println("0^6="+pow(0,6));
 	}
 
 	public static int pow(int base, int exponet)
