f0db22f638ed19909a4ed3096776c5a5cd793b0e
Victor Delaplaine <vdelaplaines@aol.com>
2019-04-15 23:37:58 -0700

    autoadd

--- labs/lab1/part4/exponet.asm
+++ labs/lab1/part4/exponet.asm
@@ -63,8 +63,8 @@ output:
 #	           out base^exponet
 # Parameters : n/a
 # Return : n/a
-# Tweaked Parmeter : $a1 = 
-#		     $a2 = div
+# Locals : $t0 - base
+#        : $t1 - exponet
 ############################################################
 main:
 #==================I/O=================
@@ -77,8 +77,8 @@ main:
 	addi $v0, $zero, 5	
 	syscall 	
 			
-	# $a0 = base
-	add $a0, $v0, $zero 
+	# $t0 - base
+	add $t0, $v0, $zero 
 
 	# Ask user to enter exponet
 	la $a0, promptExp
@@ -89,14 +89,19 @@ main:
 	addi $v0, $zero, 5	
 	syscall 	
 
-	# $a1 = exponet
-	add $a0, $v0, $zero 
+	# $t1 = exponet
+	add $t1, $v0, $zero 
+
+	#pass arguments
+	add $a0, $t0, $zero
+	add $a1, $t1, $zero
 #=============================================	
 	# call function pow(base,exponet)
 	jal pow
 	
 	# Output the result
 	add $a0, $v0, $zero
+
 	addi $v0, $zero, 1
 	syscall
 	
@@ -128,10 +133,13 @@ main:
 multiply:
 	add $t0, $zero $zero # prod = 0	
 	add $t1, $zero $zero # i = 0
+	#check if num2 == 0 first
+	beq $a2, $zero, end_multiply
 	
 loop_multiply: 
-	slt $t2, $a1, $t1 #$t2 = 1, if (num2 < i)
-			  #$t2 = 0, if (num2 >= i)
+	#check general case
+	slt $t2, $t1, $a1 #$t2 = 1, if (num2 > i)
+			  #$t2 = 0, if (i >= num2)
 	beq $t2, $zero, end_multiply #goto end_multiply	
 	add $t0, $t0, $a0 #prod = prod + num1
 	addi $t1, $t1, 1 # i++	
@@ -141,15 +149,18 @@ end_multiply:
 	add $v0, $t0, $zero
 	jr $ra #return 
 		
+	
+	
 ############################################################
-# multiply subroutine: returns the exponet of base^exponet
+# pow subroutine: returns the exponet of base^exponet
 # Parameters : $a0 - base
 #	     : $a1 - exponet
-# Return : $v0 - base^exponet
-# Tweaked Parmeter : $t0 - result
-#		   : $t1 - i
+# Return  : $v0 - base^exponet
+# locals	   : $t0 - result
 #		   : $t2 - for condition check
 #		   : $t3 - hold base
+#		   : $t4 - holds exponet
+# saved : $s0 : i
 ############################################################
 ## Java Code:
 #public static int pow(int base, int exponet)
@@ -167,18 +178,20 @@ end_multiply:
 #}
 ########################################################
 pow:
-	bne $a1, $zero, pow_loop #if exp != 0 then jump to loop else return 1
+	bne $a1, $zero, pow_init #if exp != 0 then jump to loop else return 1
 	addi $v0, $zero, 1 #return 1
 	jr $ra #return
 
+pow_init:
 	#initalize result = base
-	add $t0, $a0, $zero 
-	addi $t1, $t1, 1 #i=1
+	add $t0, $a0, $zero #$t0 = result 
+	addi $s0, $zero, 1 #$s0 = i = 1
 	add $t3, $zero, $a0 # $t3 = base
+	add $t4, $zero, $a1 # $t4 = exponet
 
 pow_loop:
 	#check if i < exponet
-	slt $t2, $t1, $a1 # $t2 = 1 if (i < exponet)
+	slt $t2, $s0, $t4 # $t2 = 1 if (i < exponet)
 			  # $t2 = 0 if (i >= exponet)
 	beq $t2, $zero, end_pow # goto end_pow
 
@@ -187,20 +200,25 @@ pow_loop:
 	sw $ra, 0($sp)
 
 	# calling function	
-	add $a0, $v0, $zero	
-	add $a1, $t3, $zero #multiply(result, base)
+	add $a0, $t0, $zero #$a0 = result 
+	add $a1, $t3, $zero #$a1 = base 
 	jal multiply # call the function
-	
+
+	#mov return of mult to result
+	add $t0, $v0, $zero #result = mult(result, base)
+		
 	# pop return address
 	lw $ra, 0($sp)
 	addi $sp, $sp, 4
-	
+
+	addi $s0, $s0, 1 #i++	
 	j pow_loop
 
 end_pow: 
+	add $v0, $t0, $zero 
 	jr $ra #return to address
 		
 		
 	
 	
-	 
+	
