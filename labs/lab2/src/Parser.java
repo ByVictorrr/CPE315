2261fefd57698b6d6733a7ee5328c7e1f49a2356
Victor Delaplaine <vdelaplaines@aol.com>
2019-04-20 15:45:18 -0700

    autoadd

--- labs/lab2/src/Parser.java
+++ labs/lab2/src/Parser.java
@@ -1,47 +1,46 @@
-/*
- * Instructions.java
- * Copyright (C) 2019 victor <victor@TheShell>
- *
- * Distributed under terms of the MIT license.
- */
-
+/* * Instructions.java * Copyright (C) 2019 victor <victor@TheShell> * * Distributed under terms of the MIT license. */
 import java.io.IOException;
 import java.util.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.regex.Pattern;
+
 
 public class Parser
 {
+
+	//=================vars=====================\\
 	public List <String> lines = new ArrayList<String>();
 
-	public static void main(String [] args) {
-		
-	}
-
+	//=================Function=====================\\
 	public void filterCommentsWhites(String [] args)
 	{
-		
-	    try(Stream<String> stream = Files.lines(Paths.get(args[1]))){
-	    	//============Filter white spaces (\t, \s, 
+
+		Pattern whiteSpaceFilter = Pattern.compile("[^\\s].*$");
+
+	    try(Stream<String> stream = Files.lines(Paths.get(args[0]))){
+	    	//============Filter white spaces (\t, \s, ..)=========\\
 	    	//Case 1: filter out blank spaces
+			lines = stream
+					.filter(whiteSpaceFilter.asPredicate())  //filter every that is not made fully of white sapce chars
+					.collect( Collectors.toList());
 
-
+			/*
 	    	//=============Filter comments out=====================\\
 			//case 2 filter comments out
 			//case 2.1: filter every line out starting with #
-			lines = stream
+			lines = lines.stream()
 					.filter( lines -> lines.startsWith("#"))
 					.collect( Collectors.toList());
-			//case 2.2: filter substrings after #
-			for (int i = 0; i< lines.size(); i++)
+			//case 2.2: filter substrings after # for (int i = 0; i< lines.size(); i++)
 			{
 				String filtered =  lines.get(i).substring(0, lines.get(i).indexOf(" #")) ;
 				lines.set(i, filtered);  //deletes everything after #
 			}
 			//====================================================\\
-
+*/
 
 			
 		}
---
+++ labs/lab2/src/driver.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.regex.Pattern;
+
+
+public class driver
+{
+
+	public static void main(String [] args) {
+		Parser parse = new Parser();
+
+		parse.filterCommentsWhites(args);
+			parse.lines.forEach(System.out::println);
+	}
+
+}
+
