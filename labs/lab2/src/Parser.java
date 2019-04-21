85933d775d54e5d6ef7a75760b84b284f3ba17fc
Victor Delaplaine <vdelaplaines@aol.com>
2019-04-20 19:05:01 -0700

    autoadd

--- labs/lab2/src/Parser.java
+++ labs/lab2/src/Parser.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.util.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.regex.Pattern;
@@ -11,7 +12,7 @@ import java.util.regex.Pattern;
 public class Parser {
 	//=================vars=====================\\
 	public List<String> lines = new ArrayList<String>();
-	public Map<String,Long> label = new HashMap<>();
+	public Map<String,Long> labelMap = new HashMap<>();
 
 	//=================Function=====================\\
 	public void filterCommentsWhites(String[] args) {
@@ -34,21 +35,53 @@ public class Parser {
 		//================Filters out the comments after instructions\ labels==========\\
 		for (int i = 0; i < lines.size(); i++) {
 			if (lines.get(i).contains("#")) {
-				lines.set(i, lines.get(i).substring(0, lines.get(i).indexOf(" #")));
+				lines.set(i, lines.get(i).substring(0, lines.get(i).indexOf("#")));
 			}
 		}
 		//====================================================\\
+
+
+		//=========================Remove white spaces before in the string=========\\
+		for (int i = 0; i < lines.size(); i++) {
+				lines.set(i,lines.get(i).trim());
+		}
+		//========================================================================\\\
 	}
 
-	public boolean isLabel(String label)
-	{
-			if (label.contains(":")) { //has to at least contain :
-				lines.set(i, lines.get(i).substring(0, lines.get(i).indexOf(" :")));
-			}
-	}
 	public Map<String, Long> getLabel(Long BaseAddress){ //get Labels and filter out the
+
+		Pattern LabelFormat = Pattern.compile("[0-9a-zA-Z]+\\s+?:");
+		//get filtered String that correcponds to labels put them in a list
+		List<String> labels = lines.stream()
+									.filter(LabelFormat.asPredicate())
+									.collect(Collectors.toList());
+
+
+		//===================get address of corrersponding address of labels===================\\
 		Long Address = BaseAddress;
-		Pattern LabelFormat = Pattern.compile("");
+		List<Long> AddrList = new ArrayList<>();
+
+		for (int i = 0; i< lines.size(); i++){
+
+			if(!LabelFormat.matcher(lines.get(i)).find()) //count each time a non label is found
+			{
+				if (i != 0) { //if not first line
+					Address++;
+				}
+			}
+			AddrList
+		}
+		//=================================================================================\\
+
+
+		//=======================put label, and corresonding addr in map=================================\\
+
+		for (String label: labels) {
+			labelMap.put(label, );
+		}
+					.filter(noFrontComments.asPredicate()) //get all lines that dont start with
+					.filter(noBlankLines.asPredicate()) //get all lines that dont start with
+					.collect(Collectors.toList());
 
 		for (int i = 0; i < lines.size(); i++) {
 			if (lines.get(i).contains(":")) {
@@ -58,6 +91,5 @@ public class Parser {
 		}
 
 	}
-
 }
 
