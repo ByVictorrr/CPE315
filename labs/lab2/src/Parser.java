b53e9fd374d6baa3c1f28295158bf7335c949019
Victor Delaplaine <vdelaplaines@aol.com>
2019-04-20 18:05:01 -0700

    autoadd

--- labs/lab2/src/Parser.java
+++ labs/lab2/src/Parser.java
@@ -8,23 +8,23 @@ import java.util.stream.Stream;
 import java.util.regex.Pattern;
 
 
-public class Parser
-{
-
+public class Parser {
 	//=================vars=====================\\
-	public List <String> lines = new ArrayList<String>();
-	public int numLines = 0;
+	public List<String> lines = new ArrayList<String>();
+	public Map<String,Long> label = new HashMap<>();
 
 	//=================Function=====================\\
-	public void filterCommentsWhites(String [] args) {
+	public void filterCommentsWhites(String[] args) {
 
-		Pattern noFrontCommentsOrBlankLines = Pattern.compile("^[^\\s #].*");
+		Pattern noFrontComments = Pattern.compile("^[^#].*");
+		Pattern noBlankLines = Pattern.compile("[^\\s][a-z].*$");
 
 		//After passing these patterns its possible for therre to be # after an instruction
 		try (Stream<String> stream = Files.lines(Paths.get(args[0]))) {
 			//=============filter front of line comments and blank lines=====================\\
 			lines = stream
-					.filter(noFrontCommentsOrBlankLines.asPredicate()) //get all lines that dont start with
+					.filter(noFrontComments.asPredicate()) //get all lines that dont start with
+					.filter(noBlankLines.asPredicate()) //get all lines that dont start with
 					.collect(Collectors.toList());
 			//=================================================================================\\
 		} catch (IOException e) {
@@ -32,15 +32,32 @@ public class Parser
 		}
 
 		//================Filters out the comments after instructions\ labels==========\\
-		for (int i = 0; i < lines.size(); i++){
+		for (int i = 0; i < lines.size(); i++) {
 			if (lines.get(i).contains("#")) {
-				lines.set(i, lines.get(i).substring(0, lines.get(i).indexOf("#")));
+				lines.set(i, lines.get(i).substring(0, lines.get(i).indexOf(" #")));
+			}
+		}
+		//====================================================\\
+	}
+
+	public boolean isLabel(String label)
+	{
+			if (label.contains(":")) { //has to at least contain :
+				lines.set(i, lines.get(i).substring(0, lines.get(i).indexOf(" :")));
+			}
+	}
+	public Map<String, Long> getLabel(Long BaseAddress){ //get Labels and filter out the
+		Long Address = BaseAddress;
+		Pattern LabelFormat = Pattern.compile("");
+
+		for (int i = 0; i < lines.size(); i++) {
+			if (lines.get(i).contains(":")) {
+				lines.set(i, lines.get(i).substring(0, lines.get(i).indexOf(" :")));
 			}
 			numLines = i; //Use numlines for address number for the labels
-			}
-			//====================================================\\
+		}
+
 	}
-	public Map<Integer, Integer Unsigned >
 
 }
 
