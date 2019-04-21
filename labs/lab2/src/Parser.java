67fd9350f839913aacc5a39104d94d98588c09e1
Victor Delaplaine <vdelaplaines@aol.com>
2019-04-20 17:05:01 -0700

    autoadd

--- labs/lab2/src/Parser.java
+++ labs/lab2/src/Parser.java
@@ -13,43 +13,34 @@ public class Parser
 
 	//=================vars=====================\\
 	public List <String> lines = new ArrayList<String>();
+	public int numLines = 0;
 
 	//=================Function=====================\\
-	public void filterCommentsWhites(String [] args)
-	{
+	public void filterCommentsWhites(String [] args) {
 
-		Pattern whiteSpaceFilter = Pattern.compile("[^\\s].*$");
+		Pattern noFrontCommentsOrBlankLines = Pattern.compile("^[^\\s #].*");
 
-	    try(Stream<String> stream = Files.lines(Paths.get(args[0]))){
-	    	//============Filter white spaces (\t, \s, ..)=========\\
-	    	//test1.1: filter out blank spaces
+		//After passing these patterns its possible for therre to be # after an instruction
+		try (Stream<String> stream = Files.lines(Paths.get(args[0]))) {
+			//=============filter front of line comments and blank lines=====================\\
 			lines = stream
-					.filter(whiteSpaceFilter.asPredicate())  //filter every that is not made fully of white sapce chars
-					.collect( Collectors.toList());
-
-		}
-		catch (IOException e)
-		{
+					.filter(noFrontCommentsOrBlankLines.asPredicate()) //get all lines that dont start with
+					.collect(Collectors.toList());
+			//=================================================================================\\
+		} catch (IOException e) {
 			e.getSuppressed();
 		}
 
-	    	//=============Filter comments out=====================\\
-			//test 2 filter comments out
-			//test 2.1: filter every line out starting with #
-			lines = lines.stream()
-					.filter( lines -> !lines.startsWith("#"))
-					.collect( Collectors.toList());
-
-			//test 2.2: filter substrings after # for (int i = 0; i< lines.size(); i++)
-			lines.forEach( s-> {
-				s = s.substring(0, s.indexOf(" #")) ;
-			});
+		//================Filters out the comments after instructions\ labels==========\\
+		for (int i = 0; i < lines.size(); i++){
+			if (lines.get(i).contains("#")) {
+				lines.set(i, lines.get(i).substring(0, lines.get(i).indexOf("#")));
+			}
+			numLines = i; //Use numlines for address number for the labels
+			}
 			//====================================================\\
-
 	}
-
-
-
+	public Map<Integer, Integer Unsigned >
 
 }
 
